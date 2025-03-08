98f3f6c3902c9c13be46a98493903e849ba18b45
Testup476 <Kandajeanmarc@gmail.com>
2025-03-08 14:49:01 +0100

    FileChanged

---
+++ scripts/getdates.js
@@ -0,0 +1,2 @@
+document.getElementById("currentyear").textContent = "©" + new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified : " + document.lastModified;
