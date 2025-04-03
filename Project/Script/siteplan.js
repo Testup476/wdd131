0cc69a36fd3611799acdf4d836f8b79119f64977
Testup476 <Kandajeanmarc@gmail.com>
2025-04-03 14:30:12 +0100

    Newchange

---
+++ Project/Script/siteplan.js
@@ -0,0 +1,10 @@
+
+document.getElementById("desktop").addEventListener('click', () => {
+    window.open("https://wireframe.cc/1IBOfi");
+
+});
+
+
+document.getElementById("mobile").addEventListener('click', () => {
+    window.open("https://wireframe.cc/6NByNA");
+});
